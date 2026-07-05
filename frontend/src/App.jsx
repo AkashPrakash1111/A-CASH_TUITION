a2d9a2d8f5bb07283bc011099a2acaf6ad59a22d
AkashPrakash1111 <akashsprakash1111@gmail.com>
2026-07-05 10:17:47 +0530

    v0.1.0 - React and Tailwind project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">
+          ACASH Tuition Portal
+        </h1>
+
+        <p className="mt-5 text-gray-400">
+          🚀 React + Tailwind is working!
+        </p>
+      </div>
+    </div>
+  );
+}
